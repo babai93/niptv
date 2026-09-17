f346046c743e61f642f9ab47c80878c97ecbf422
Somnath Dutta <babai.dutta.g@gmail.com>
2026-09-17 10:45:12 +0530

    niptv

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ const HTML_UI = `
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20" class="text-blue-500 hidden md:block flex-shrink-0">
                     <path fill="currentColor" d="M64 64C28.7 64 0 92.7 0 128L0 352c0 35.3 28.7 64 64 64l112 0-10.7 32L128 448c-17.7 0-32 14.3-32 32s14.3 32 32 32l256 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-37.3 0L336 416l112 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L64 64zm0 64l384 0 0 224L64 288l0-160z"/>
                 </svg>
-                <span>IPTV Player</span>
+                <span>NIPTV Player</span>
             </h1>
             <button type="button" id="closeSidebarBtn" class="md:hidden p-2 rounded-lg hover:bg-slate-800 active:bg-slate-800" aria-label="Close channel list">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="16" height="16">
@@ -591,7 +591,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log('IPTV Player running at http://localhost:' + PORT);
+    console.log('NIPTV Player running at http://localhost:' + PORT);
 });
 
 app.get('/api/metadata', async (req, res) => {
